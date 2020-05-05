84a505ecbf0efa9b3c8cee6c1d87327cbe1e14e4
seetharamulu <thumurugoti.seetharamulu@gmail.com>
2020-05-05 15:02:38 +0530

    added email_check.sh

--- email_check.sh
+++ email_check.sh
@@ -1,7 +1,7 @@
 #! /bin/bash/
 email=$1
 
-if [[ $email =~ ^[a-zA-Z0-9]+([._+-][0-9a-zA-Z]+)*\@[a-z]{1,}\.[a-z]{1,4}([\.][a-z]{1,2}$) ]]
+if [[ $email =~ ^[a-zA-Z0-9]+([._+-][0-9a-zA-Z]+)*\@[a-z]{1,}\.[a-z]{1,4}$ ]]
 then
    echo "valid"
 else
